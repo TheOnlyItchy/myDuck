441bd8bbc079e61ddb06d9392eef0fa468773a76
SuperChez <sakul.nosnhoj@gmail.com>
2022-05-18 10:49:19 -0800

    wrote some stuff

--- Guessing Game/guessingScript.js
+++ Guessing Game/guessingScript.js
@@ -1,5 +1,11 @@
 function generateRandomNumber() {
     var val = Math.floor(Math.random() * 100);
-    console.log("Target Number: " + val);
     return val;
 }
+
+var target = generateRandomNumber();
+console.log("Target Number: " + target);
+
+// grab the form input
+// compare it to the target
+// determine whether it's higher lower or right

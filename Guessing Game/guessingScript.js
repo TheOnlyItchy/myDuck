ac11e159b4367048845d3e9165c746c4be1d8a20
SuperChez <sakul.nosnhoj@gmail.com>
2022-05-23 10:58:31 -0800

    wrote some bad js

--- Guessing Game/guessingScript.js
+++ Guessing Game/guessingScript.js
@@ -1,3 +1,7 @@
+function game() {
+    
+}
+
 function generateRandomNumber() {
     var val = Math.floor(Math.random() * 100);
     return val;
@@ -9,3 +13,14 @@ console.log("Target Number: " + target);
 // grab the form input
 // compare it to the target
 // determine whether it's higher lower or right
+
+function play() {
+    var guess;
+    if(guess == target) {
+        // success call
+    } else if(guess < target) {
+        // less than call
+    } else {
+        // greater than call
+    }
+}
